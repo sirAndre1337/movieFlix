3c9ba800bff578905228978a866b97edf74297bf
André Luis <andrelluis17@hotmail.com>
2022-03-01 23:03:54 -0300

    Entidades criadas, seed do banco

---
+++ backend/src/test/java/com/andre/MovieflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
