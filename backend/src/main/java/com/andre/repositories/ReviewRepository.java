c3588da8131dd106a86193d8abdf5acf4e8c9fa3
André Luis <andrelluis17@hotmail.com>
2022-03-02 11:00:09 -0300

    Repositories , Security

---
+++ backend/src/main/java/com/andre/repositories/ReviewRepository.java
@@ -0,0 +1,11 @@
+package com.andre.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.andre.entities.Review;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long>{
+
+}
