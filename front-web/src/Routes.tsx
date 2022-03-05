320bf356d4a787f6fc538c18ab3d64cc58d78d8a
André Luis <andrelluis17@hotmail.com>
2022-03-05 15:13:23 -0300

    Logout impl

--- front-web/src/Routes.tsx
+++ front-web/src/Routes.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import Navbar from "./core/components/Navbar";
 import Home from "./pages/Home";
 import Movie from "./pages/Movie";
@@ -7,6 +7,7 @@ const Routes = () => (
     <BrowserRouter>
         <Navbar />
         <Switch>
+        <Redirect from='/home' to="/" exact/>
             <Route path="/" exact>
                 <Home />
             </Route>
--- front-web/src/core/components/Navbar/index.tsx
+++ front-web/src/core/components/Navbar/index.tsx
@@ -1,14 +1,44 @@
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Link, useHistory, useLocation } from 'react-router-dom';
+import { getAccessTokenDecode, logout } from '../../utils/auth';
 import './styles.scss';
 
-const Navbar = () => (
-    <nav className='row bg-primary main-nav'>
-        <div className='col-2'>
-            <Link to="/" className='nav-logo-text'>
-                <h4>MovieFlix</h4>
-            </Link>        
-        </div>
-    </nav>
-);
+const Navbar = () => {
+
+    const [user, setUser] = useState('');
+    const location = useLocation();
+    const history = useHistory();
+    
+    useEffect(() => {
+        const userData = getAccessTokenDecode();
+        setUser(userData.user_name);
+    }, [location])
+    
+    const handleLogout = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+        event.preventDefault();
+        logout();
+        history.push('/home')
+    }
+
+    return (
+        <nav className='row bg-primary main-nav'>
+            <div className='col-10'>
+                <Link to="/" className='nav-logo-text'>
+                    <h4>MovieFlix</h4>
+                </Link>
+            </div>
+            {user &&
+                <div className='col-2 nav-logout'>
+                    <a
+                        href="#logout"
+                        onClick={handleLogout}
+                    >
+                        Sair
+                    </a>
+                </div>
+            }
+        </nav>
+    )
+};
 
 export default Navbar;
